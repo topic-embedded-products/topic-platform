77b0301640ec663ee4a6b989f55fe459ca24c6bf
Mike Looijmans <mike.looijmans@topic.nl>
2020-11-23 16:29:03 +0100

    scripts/autobuild.sh: Create versioned artefacts
    Put a git version hash in the filename. So artefacts obtained from the
    build server are tracable.
    
    Keep a non-versioned artefact name as well so as to not break the release
    creation script.

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -5,17 +5,28 @@ then
   echo "First time build - creating folders"
   scripts/init-oe.sh
 fi
+VERSION=`git rev-parse --short HEAD`
 cd build
 source ./profile
-rm -rf artefacts
+rm -rf artefacts artefacts-nv
 mkdir artefacts
+mkdir artefacts-nv
 for machine in tdkz15 tdkz30 tdkzu6 tdkzu9 tdkzu15 xdpzu7 ttpzu9 tdpzu9
 do
   export MACHINE=$machine
   nice bitbake -k my-image-feed
-  nice xz -6 < tmp-glibc/deploy/images/${MACHINE}/my-image-${MACHINE}.wic > artefacts/my-image-${MACHINE}.wic.xz &
-  cp tmp-glibc/deploy/images/$MACHINE/*-$MACHINE.swu artefacts/
+  nice xz -6 < tmp-glibc/deploy/images/${MACHINE}/my-image-${MACHINE}.wic > artefacts-nv/my-image-${MACHINE}.wic.xz &
+  cp tmp-glibc/deploy/images/$MACHINE/*-$MACHINE.swu artefacts-nv/
 done
 
 # wait for XZ compressors
 wait
+
+for ext in wic.xz swu
+do
+  for a in artefacts-nv/*.${ext}
+  do
+    b=`basename "${a}" .${ext}`
+    ln "${a}" "artefacts/${b}+${VERSION}.${ext}"
+  done
+done
--- scripts/release-downloads.sh
+++ scripts/release-downloads.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 cd build
 # Update images on external web server (downloads.topic.nl)
-s3cmd put --acl-public --no-progress artefacts/* s3://topic-downloads/images/
+s3cmd put --acl-public --no-progress artefacts-nv/* s3://topic-downloads/images/
