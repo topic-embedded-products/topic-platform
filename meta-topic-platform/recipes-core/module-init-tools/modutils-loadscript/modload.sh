0e323835ac70e0cc07027eec1484b4d77e7efd11
Mike Looijmans <mike.looijmans@topic.nl>
2022-05-11 16:49:12 +0200

    modutils-loadscript: Load modules unconditionally
    Remove the filter on known hotplug devices. With devicetree overlays,
    even platform devices may appear out of the blue. So just try to load
    everything. This fixes that some modules would not automatically be
    activated at boot, like the gpio-xilinx and xlnx-iic modules.

--- meta-topic-platform/recipes-core/module-init-tools/modutils-loadscript/modload.sh
+++ meta-topic-platform/recipes-core/module-init-tools/modutils-loadscript/modload.sh
@@ -2,8 +2,8 @@
 # Licensed under GPLv2
 # Load kernel modules for hotpluggable devices
 
-# Probe all USB/HID/input modules so that drivers load automatically
-find /sys/devices/ -name modalias -print0 | xargs -0 grep -h '^usb:\|^input:\|^hid:' | while read m
+# Probe all modules so that drivers load automatically
+find /sys/devices/ -name modalias -print0 | xargs -0 grep -h ':' | while read m
 do
 	modprobe $m > /dev/null 2> /dev/null
 done
