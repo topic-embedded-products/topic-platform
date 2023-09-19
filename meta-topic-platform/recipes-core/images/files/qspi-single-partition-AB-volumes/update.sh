b7ee54ff83bdaf9e6536920dd4128ebf65181580
Mike Looijmans <mike.looijmans@topic.nl>
2023-09-19 09:40:46 +0200

    images: Preserve files on QSPI (UBI) AB-volumes configuration
    Implement preserving files in /etc/swu-transfer-list for ubifs systems
    in the QSPI.
    Only implemented for the "single-partition-AB-volumes" configuration, as
    it needs access to both new and old volumes.

--- meta-topic-platform/recipes-core/images/files/qspi-single-partition-AB-volumes/update.sh
+++ meta-topic-platform/recipes-core/images/files/qspi-single-partition-AB-volumes/update.sh
@@ -12,6 +12,18 @@ then
 	ubiattach -m 1 /dev/ubi_ctrl || true
 elif [ "$CMD" = "postinst" ]
 then
+	if [ -s /etc/swu-transfer-list ]
+	then
+		rmdir /tmp/UPDATE-MOUNT 2> /dev/null || true
+		if mkdir /tmp/UPDATE-MOUNT && mount -t ubifs ubi0:qspi-rootfs /tmp/UPDATE-MOUNT
+		then
+			swu-transfer-settings /tmp/UPDATE-MOUNT || true
+			umount /tmp/UPDATE-MOUNT
+			rmdir /tmp/UPDATE-MOUNT || true
+		else
+			echo "Cannot transfer settings"
+		fi
+	fi
 	reboot
 else
 	echo "Invalid command"
