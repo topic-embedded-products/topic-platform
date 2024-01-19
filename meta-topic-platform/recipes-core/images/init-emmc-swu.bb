01274ca351b7a7267ae9c8e6ac9d226ec715ba35
Mike Looijmans <mike.looijmans@topic.nl>
2024-01-19 13:16:46 +0100

    meta-topic: Add tepzu9 board
    Add support for a new "tepzu9" carrier and adapt build scripts

--- meta-topic-platform/recipes-core/images/init-emmc-swu.bb
+++ meta-topic-platform/recipes-core/images/init-emmc-swu.bb
@@ -3,7 +3,7 @@ require init-common-swu.inc
 SUMMARY = "eMMC Bootloader & partition init package for SWUpdate"
 
 # Only these machines have eMMC devices
-COMPATIBLE_MACHINE = "^td[kp]zu|^xdp|^ttp"
+COMPATIBLE_MACHINE = "^t[de][kp]zu|^xdp|^ttp"
 
 FILESEXTRAPATHS:prepend := "${THISDIR}/files/init-emmc-${SWU_BOOT_TYPE}:"
 
--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -11,12 +11,12 @@ source ./profile
 rm -rf artefacts artefacts-nv
 mkdir artefacts
 mkdir artefacts-nv
-for machine in tdkz15 tdkz30 tdpzu9 tdkzu6 tdkzu9 tdkzu15
+for machine in tdkz15 tdkz30 tepzu9 tdpzu9 tdkzu6 tdkzu9 tdkzu15
 do
   export MACHINE=$machine
   nice bitbake -k my-image-feed
   nice xz -6 < tmp-glibc/deploy/images/${MACHINE}/my-image-${MACHINE}.rootfs.wic > artefacts-nv/my-image-${MACHINE}.wic.xz &
-  cp tmp-glibc/deploy/images/$MACHINE/*-$MACHINE.rootfs.swu artefacts-nv/
+  cp -l tmp-glibc/deploy/images/$MACHINE/*-$MACHINE.rootfs.swu artefacts-nv/
 done
 
 # wait for XZ compressors
