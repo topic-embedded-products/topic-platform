71ca7e0c13d250956d754726a66f5835fd312edd
Mike Looijmans <mike.looijmans@topic.nl>
2025-05-21 14:54:11 +0200

    topic-usb-cfg: Enable serial port on USB gadget
    For some testing, add an ACM port (serial port at USB speeds)

--- meta-topic-platform/recipes-topic/service/topic-usb-cfg/gadget_config.sh
+++ meta-topic-platform/recipes-topic/service/topic-usb-cfg/gadget_config.sh
@@ -19,7 +19,7 @@ fi
 mkdir $g1
 mkdir $g1/strings/0x409 # US English, others rarely seen
 echo "TOPIC" > $g1/strings/0x409/manufacturer
-echo "Network gadget" > $g1/strings/0x409/product
+echo "Network+Serial gadget" > $g1/strings/0x409/product
 if [ -e /etc/swrevision ]
 then
 	cat /etc/swrevision > $g1/strings/0x409/serialnumber
@@ -33,10 +33,13 @@ echo "0x1d6b" > $g1/idVendor
 echo "0x0104" > $g1/idProduct
 # Ethernet
 mkdir $g1/functions/${ethmode}.usb0
+# Serial
+mkdir $g1/functions/acm.0
 
 # Configuration
 mkdir $g1/configs/c1.1
 ln -s $g1/functions/${ethmode}.usb0 $g1/configs/c1.1/${ethmode}.usb0
+ln -s $g1/functions/acm.0 $g1/configs/c1.1/acm.0
 
 # Attach to UDC
 echo $f > $g1/UDC
